fe33bde74761e78191dee432c0520574ccca1486
a-rom <a1623at@aiit.ac.jp>
2016-08-06 11:25:02 +0000

    messageメソッドのテストコードを修正しました

--- test/gem_team1misson2_test.rb
+++ test/gem_team1misson2_test.rb
@@ -2,17 +2,17 @@ require 'test_helper'
 
 class GemTeam1misson2Test < Minitest::Test
   def setup
-    @main = ::GemTeam1misson2Test::Main.new
+    @main = Main.new
   end
 
   def test_that_it_has_a_version_number
     refute_nil ::GemTeam1misson2::VERSION
   end
 
-  def message
-   assert_equal false, @main.massage(15), "good"
-   assert_equal false, @main.massage(30), "bad"
-   assert_equal false, @main.massage(5),  "good"
-   assert_equal false, @main.massage(100),"bad"
+  def test_message 
+   assert_equal "good", @main.message(169,30)
+   assert_equal "bad", @main.message(169,70)
+   assert_equal "good", @main.message(180,50)
+   assert_equal "bad", @main.message(175,70)
   end
 end
