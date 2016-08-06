0d5d1dd6712bb0f28817c5334543c439ee828f4e
a-rom <a1623at@aiit.ac.jp>
2016-08-06 10:06:29 +0000

    gem_team1misson2_test.rbを修正しました

--- test/gem_team1misson2_test.rb
+++ test/gem_team1misson2_test.rb
@@ -1,11 +1,18 @@
 require 'test_helper'
 
 class GemTeam1misson2Test < Minitest::Test
+  def setup
+    @main = ::GemTeam1misson2Test::Main.new
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::GemTeam1misson2::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def message
+   assert_equal false, @main.massage(15), "good"
+   assert_equal false, @main.massage(30), "bad"
+   assert_equal false, @main.massage(5),  "good"
+   assert_equal false, @main.massage(100),"bad"
   end
 end
