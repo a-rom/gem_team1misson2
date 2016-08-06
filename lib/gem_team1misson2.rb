da5bf6b846f3513a59ae1c378e82352f274ff55a
a-rom <a1623at@aiit.ac.jp>
2016-08-06 10:35:29 +0000

    gem_team1misson2.rbに全角スペースが入ってしまっていたので、修正しました

--- lib/gem_team1misson2.rb
+++ lib/gem_team1misson2.rb
@@ -1,5 +1,4 @@
 require "gem_team1misson2/version"
-
 class Main
 def bmi?(height, weight)
   s = weight / ( height / 100.0 ) ** 2
@@ -9,7 +8,7 @@ end
 def message(height, weight)
   t = bmi?(height, weight)
    if t < 20 
-   　puts "good" 
+     puts "good" 
    else
      puts "bad"
    end
@@ -19,4 +18,4 @@ height = ARGV[0].to_i
 weight = ARGV[1].to_i 
 
 message(height, weight)
-
+end
