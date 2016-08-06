a4c3615c4c8e4b781ead877285bd4dc9e0903687
None <stok20010711@gmail.com>
2016-08-06 10:32:31 +0000

    test1

--- test/gem_team1misson2_test.rb
+++ test/gem_team1misson2_test.rb
@@ -9,6 +9,11 @@ class GemTeam1misson2Test < Minitest::Test
     refute_nil ::GemTeam1misson2::VERSION
   end
 
+  def test_bmi?()
+   assert_equal 12.5 , @main.bmi?(50,2) ,nil
+  end
+
+
   def message
    assert_equal false, @main.massage(15), "good"
    assert_equal false, @main.massage(30), "bad"
