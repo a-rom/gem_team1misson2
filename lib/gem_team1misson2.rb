2368dc3c1db294814e9b6e27d31ffec042d5ccb3
a-rom <a1623at@aiit.ac.jp>
2016-08-08 14:25:37 +0000

    四捨五入して小数点２位まで表示するよう修正。それに伴いテストファイルも修正しました

--- lib/gem_team1misson2.rb
+++ lib/gem_team1misson2.rb
@@ -2,20 +2,20 @@ require "gem_team1misson2/version"
 class Main
 def bmi?(height, weight)
   s = weight / ( height / 100.0 ) ** 2
-  return s
+  return s.round(2)
 end
 
 def message(height, weight)
   t = bmi?(height, weight)
-   if t < 20 
+   if t < 20.0 
      return "good" 
    else
      return "bad"
    end
 end   
 
-height = ARGV[0].to_i 
-weight = ARGV[1].to_i 
+height = ARGV[0].to_f
+weight = ARGV[1].to_f
 
 main = Main.new
 main.message(height, weight)
--- test/gem_team1misson2_test.rb
+++ test/gem_team1misson2_test.rb
@@ -10,14 +10,17 @@ class GemTeam1misson2Test < Minitest::Test
   end
 
   def test_message 
-   assert_equal "good", @main.message(169,30)
-   assert_equal "bad", @main.message(169,70)
-   assert_equal "good", @main.message(180,50)
-   assert_equal "bad", @main.message(175,70)
+   assert_equal "good", @main.message(169.0,30.0)
+   assert_equal "bad", @main.message(169.0,70.0)
+   assert_equal "good", @main.message(180.0,50.0)
+   assert_equal "bad", @main.message(175.0,70.0)
   end
 
   def test_bmi?()
-   assert_equal 8.0 , @main.bmi?(50,2)
+   assert_equal 21.75 , @main.bmi?(200.0,87.0)
+   assert_equal 22.06 , @main.bmi?(169.0,63.0)
+   assert_equal 18.9 , @main.bmi?(156.0,46.0)
+   assert_equal 14.53 , @main.bmi?(176.0,45.0)
   end
 end
 
